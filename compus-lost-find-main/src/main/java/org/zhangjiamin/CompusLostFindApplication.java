db6daab93d19354a8c2d5de7e780c0ae112aaf40
张家铭 <3843300436@qq.com>
2026-06-25 21:27:19 +0800

    init

--- compus-lost-find-main/src/main/java/org/zhangjiamin/CompusLostFindApplication.java
+++ compus-lost-find-main/src/main/java/org/zhangjiamin/CompusLostFindApplication.java
@@ -2,12 +2,17 @@ package org.zhangjiamin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.zhangjiamin.util.AliyunOSSUtil;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AliyunOSSUtil.class)
 public class CompusLostFindApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CompusLostFindApplication.class, args);
     }
+
+
 
 }
